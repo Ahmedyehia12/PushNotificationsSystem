5cc4947affcfcba515c3d73b54ea95224d08df8a
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-12-01 10:48:23 +0200

    removed unecessary imports

--- lib/screens/chat_room_page.dart
+++ lib/screens/chat_room_page.dart
@@ -9,22 +9,21 @@ class ChatRoomPage extends StatefulWidget {
   @override
   State<ChatRoomPage> createState() => _ChatRoomPageState();
 }
-
 class _ChatRoomPageState extends State<ChatRoomPage> {
   final TextEditingController _messageController = TextEditingController();
   final DatabaseReference _messagesRef =
-      FirebaseDatabase.instance.ref().child('messages');
+      FirebaseDatabase.instance.ref().child('messages');   // this is the reference to the Firebase Realtime Database instance in the messages node
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.channelName),
+        title: Text(widget.channelName),  // this is the name of the channel
       ),
       body: Column(
         children: <Widget>[
           Expanded(
-            child: StreamBuilder(
-              stream: _messagesRef.child(widget.channelName).onValue,
+            child: StreamBuilder( // streamBuilder widget listens to the stream of messages in the channel
+              stream: _messagesRef.child(widget.channelName).onValue, // the value to be updated in the stream
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
                   return const Center(
@@ -32,17 +31,17 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
                   );
                 }
                 final messages = <String>[];
-                final data = snapshot.data?.snapshot.value as Map<dynamic, dynamic>?;
+                final data = snapshot.data?.snapshot.value as Map<dynamic, dynamic>?; // snapshot: the data that is being streamed from the database
                 if (data != null) {
                   data.forEach((key, value) {
-                    messages.add(value['message'] as String);
+                    messages.add(value['message'] as String); // here we are adding the message to the list of messages
                   });
                 }
-                return ListView.builder(
-                  itemCount: messages.length,
+                return ListView.builder(  // this is the list of messages
+                  itemCount: messages.length, // the number of messages
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Text(messages[index]),
+                      title: Text(messages[index]), // the message
                     );
                   },
                 );
@@ -64,10 +63,10 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
                 IconButton(
                   icon: const Icon(Icons.send),
                   onPressed: () {
-                    _messagesRef.child(widget.channelName).push().set({
-                      'message': _messageController.text,
-                      'userId': widget.userId,
-                    });
+                    _messagesRef.child(widget.channelName).push().set({ // this is the message that is being sent to the database, to the /messages/channelName node, this will rebuild the streamBuilder widget
+                      'message': _messageController.text, // the message
+                      'userId': widget.userId, // the user id
+                    }); 
                     _messageController.clear();
                   },
                 ),
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'chat_room_page.dart';
 
 class HomePage extends StatefulWidget {
